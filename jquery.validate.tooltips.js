a2c4c372ea6f56575e9f998a548398f7c7293a7e
Shaun <simshaun@gmail.com>
2011-11-11 07:21:26 -0500

    Initial commit

---
+++ jquery.validate.tooltips.js
@@ -0,0 +1,119 @@
+/**
+ * jQuery Validation Tooltips Plugin
+ *
+ * http://envysphere.com/jquery-validation-tooltips-14/
+ *
+ * Copyright (c) 2011 Shaun Simmons
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+;(function($) {
+	// Extra spacing is applied only in the direction of the error's side closest
+	// to it's element.
+	var extra_spacing = {
+		top:    2,
+		right:  3,
+		bottom: 2,
+		left:   3
+	};
+
+	$.extend($.validator.defaults, {
+		errorPlacement: function($error, $element) {
+			$error.css('visibility', 'hidden').appendTo($element[0].form);
+		},
+
+		showErrors: function(errorMap, errorList) {
+			this.defaultShowErrors();
+			this.reflow(errorList);
+		}
+	});
+
+	$.validator.prototype.reflow = function(errorList) {
+		// If an errorList is not provided and the form has already been submitted once,
+		// reflow all errors.
+		if ( ! errorList) {
+			if (typeof this.lastElement !== 'undefined') {
+				this.reset();
+				this.form();
+			}
+
+			return;
+		}
+
+		var i = errorList.length;
+		while (i--) {
+			var $element = $(errorList[i].element),
+				$error = this.errorsFor(errorList[i].element),
+				error_position = [$element.data('error-position') || 'r'][0].toLowerCase(),
+				offset, top = 0, left = 0;
+
+			switch (errorList[i].element.type) {
+				// Make an assumption that radios and checkboxes are wrapped by a label, or
+				// they have labels that are directly linked using the `for` attribute.
+				case 'checkbox':
+				case 'radio':
+					var $parentLabel = $element.parent('label');
+					if ($parentLabel.length) {
+						$element = $parentLabel;
+						top = -3;
+					}
+					else {
+						var $linkedLabel = $(this.currentForm).find("label[for='"+ $element.attr('id') +"']");
+						if ($linkedLabel.length) {
+							$element = $linkedLabel;
+							top = -3;
+						}
+					}
+					break;
+			}
+
+			offset = $element.offset();
+
+			switch (error_position) {
+				case 't':
+					top += offset.top - extra_spacing.top - $element.outerHeight();
+					left += offset.left;
+					break;
+				case 'b':
+					top += offset.top + extra_spacing.bottom + $element.outerHeight();
+					left += offset.left;
+					break;
+				case 'l':
+					top += offset.top;
+					left += offset.left - extra_spacing.right - $error.outerWidth();
+					break;
+				case 'tl':
+					top += offset.top - extra_spacing.top - $element.outerHeight();
+					left += offset.left - extra_spacing.right - $error.outerWidth();
+					break;
+				case 'tr':
+					top += offset.top - extra_spacing.top - $element.outerHeight();
+					left += offset.left + extra_spacing.left + $element.outerWidth();
+					break;
+				case 'bl':
+					top += offset.top + extra_spacing.bottom + $element.outerHeight();
+					left += offset.left - extra_spacing.right - $error.outerWidth();
+					break;
+				case 'br':
+					top += offset.top + extra_spacing.bottom + $element.outerHeight();
+					left += offset.left + extra_spacing.left + $element.outerWidth();
+					break;
+				case 'r':
+				default:
+					top += offset.top;
+					left += offset.left + extra_spacing.left + $element.outerWidth();
+					break;
+			}
+
+			$error.css({
+				'left': left + 'px',
+				'top': top + 'px',
+				'position': 'absolute',
+				'visibility': 'visible'
+			});
+		}
+	};
+})(jQuery);
